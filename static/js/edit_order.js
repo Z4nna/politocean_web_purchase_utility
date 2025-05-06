324441c8a83a128eacec46736ee90aa01290e821
Z4nna <84254228+Z4nna@users.noreply.github.com>
2025-05-06 10:44:35 +0200

    Improved readability when creating end editing orders

--- static/js/edit_order.js
+++ static/js/edit_order.js
@@ -6,16 +6,11 @@ function addItemEntry(proposal = "", project = "", manifacturer = "", manifactur
     const div = document.createElement('div');
     div.className = 'item-entry';
     div.innerHTML = `
-        <label>Proposal:</label>
         <input type="text" class="item-input-fields" name="items_proposal_${itemIndex}" value="${proposal}" required />
-        <label>Project:</label>
         <input type="text" class="item-input-fields" name="items_project_${itemIndex}" value="${project}" required />
         
-        <label>Manifacturer:</label>
         <input type="text" class="item-input-fields" name="items_manifacturer_${itemIndex}" value="${manifacturer}" required />
-        <label>P.N.:</label>
         <input type="text" class="item-input-fields" name="items_manifacturer_pn_${itemIndex}" value="${manifacturerPn}" required />
-        <label>Quantity:</label>
         <input type="number" class="item-input-fields" name="items_quantity_${itemIndex}" value="${quantity}" required />
         <button type="button" class="delete-button" onclick="removeItemEntry(this)">×</button>
     `;
--- static/js/new_order.js
+++ static/js/new_order.js
@@ -6,16 +6,11 @@ function addItemEntry() {
     const div = document.createElement('div');
     div.className = 'item-entry';
     div.innerHTML = `
-        <label>Proposal:</label>
         <input type="text" name="items_proposal_${itemIndex}" value = "Sensoristica AUV" required />
-        <label>Project:</label>
         <input type="text" name="items_project_${itemIndex}" value = "Nereo" required />
         
-        <label>Manifacturer:</label>
         <input type="text" name="items_manifacturer_${itemIndex}" required />
-        <label>P.N.:</label>
         <input type="text" name="items_manifacturer_pn_${itemIndex}" required />
-        <label>Quantity:</label>
         <input type="number" name="items_quantity_${itemIndex}" value = "1" required />
         <button type="button" class="remove-button" onclick="removeItemEntry(this)">×</button>
     `;
