cbc9f56b429f472f9277be1cf22003ad278aa223
Z4nna <84254228+Z4nna@users.noreply.github.com>
2025-05-06 10:58:46 +0200

    Improved header alignment

--- static/js/edit_order.js
+++ static/js/edit_order.js
@@ -6,12 +6,12 @@ function addItemEntry(proposal = "", project = "", manifacturer = "", manifactur
     const div = document.createElement('div');
     div.className = 'item-entry';
     div.innerHTML = `
-        <input type="text" class="item-input-fields" name="items_proposal_${itemIndex}" value="${proposal}" required />
-        <input type="text" class="item-input-fields" name="items_project_${itemIndex}" value="${project}" required />
+        <input type="text" name="items_proposal_${itemIndex}" value="${proposal}" required />
+        <input type="text" name="items_project_${itemIndex}" value="${project}" required />
         
-        <input type="text" class="item-input-fields" name="items_manifacturer_${itemIndex}" value="${manifacturer}" required />
-        <input type="text" class="item-input-fields" name="items_manifacturer_pn_${itemIndex}" value="${manifacturerPn}" required />
-        <input type="number" class="item-input-fields" name="items_quantity_${itemIndex}" value="${quantity}" required />
+        <input type="text" name="items_manifacturer_${itemIndex}" value="${manifacturer}" required />
+        <input type="text" name="items_manifacturer_pn_${itemIndex}" value="${manifacturerPn}" required />
+        <input type="number" name="items_quantity_${itemIndex}" value="${quantity}" required />
         <button type="button" class="delete-button" onclick="removeItemEntry(this)">×</button>
     `;
     container.appendChild(div);
